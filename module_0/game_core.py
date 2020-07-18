2dad6f3eb5d589dadf6e28a92f980613c4c3d771
nicktsvetkov <48159584+nicktsvetkov@users.noreply.github.com>
2020-07-18 15:09:27 +0300

    Сделал импорт модулей, немного доработал алгоритм угадывания

--- module_0/game_core.py
+++ module_0/game_core.py
@@ -1,5 +1,8 @@
-def game_core_v5(number):
-   # '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+
+#  Алгоритм деления пополам (в качестве варианта ставим середину интервала, затем уменьшаем интвервал в половину в соответствующую сторону и повторяем)
+
+def game_core_v5(number, rand_num_min, rand_num_max):
+    #   Важно округлять в меньшую сторону новый вариант
     #   Функция принимает загаданное число и возвращает число попыток'''
     count = 1
     rand_min_score = rand_num_min
@@ -7,9 +10,9 @@ def game_core_v5(number):
     predict = (rand_max_score - rand_min_score)/2
     while number != predict:
         if number > predict: 
-            rand_min_score = (predict)
+            rand_min_score = (predict+1)
         elif number < predict: 
-            rand_max_score = (predict)
+            rand_max_score = (predict-1)
         predict = int((rand_max_score+rand_min_score)/2)
         count+=1
     return(count) # выход из цикла, если угадали
--- module_0/score_game.py
+++ module_0/score_game.py
@@ -1,10 +1,12 @@
-def score_game(game_core):
+import game_core
+import numpy as np
+def score_game(game_core, number, rand_num_min, rand_num_max):
     #'''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы эксперимент был воспроизводим!
     random_array = np.random.randint(rand_num_min,rand_num_max, size=(1000))
     for number in random_array:
-        count_ls.append(game_core(number))
+        count_ls.append(game_core(number, rand_num_min, rand_num_max))
     score = int(np.mean(count_ls))
-    print("Ваш алгоритм угадывает число в среднем за {score} попыток")
+    print("Ваш алгоритм угадывает число в среднем за {} попыток".format(score))
     return(score)
